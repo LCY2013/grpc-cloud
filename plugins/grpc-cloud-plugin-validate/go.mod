7b20219fee1aa68e376f862824b0f8431f94c102
扶风 <fufeng@staruniongame.com>
2023-11-18 17:33:49 +0800

    Feat: validate buf

--- plugins/grpc-cloud-plugin-validate/go.mod
+++ plugins/grpc-cloud-plugin-validate/go.mod
@@ -3,14 +3,20 @@ module github.com/LCY2013/grpc-cloud/plugins/grpc-cloud-plugin-validate
 go 1.21.3
 
 require (
+	buf.build/gen/go/bufbuild/protovalidate/protocolbuffers/go v1.31.0-20231106192134-1baebb0a1518.2 // indirect
 	github.com/LCY2013/grpc-cloud/logger v0.0.0-20231106042551-8535e6f6ca95 // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/bufbuild/protovalidate-go v0.4.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/cel-go v0.18.2 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1 // indirect
+	github.com/stoewer/go-strcase v1.3.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
 	golang.org/x/net v0.14.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	google.golang.org/genproto v0.0.0-20230110181048-76db0878b65f // indirect
 	google.golang.org/grpc v1.53.0 // indirect
 	google.golang.org/protobuf v1.31.0 // indirect
--- plugins/grpc-cloud-plugin-validate/validate.go
+++ plugins/grpc-cloud-plugin-validate/validate.go
@@ -3,9 +3,11 @@ package grpc_cloud_plugin_validate
 import (
 	"context"
 	"github.com/LCY2013/grpc-cloud/logger"
+	"github.com/bufbuild/protovalidate-go"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
 	"time"
 )
 
@@ -70,6 +72,45 @@ func WithValidateServerInterceptor(ctx context.Context,
 	// validate
 	if validate, ok := req.(interface{ ValidateAll() error }); ok {
 		if err := validate.ValidateAll(); err != nil {
+			logger.Log.Infof("Request - Method:%s\tDuration:%s\tError:%v\n",
+				info.FullMethod,
+				time.Since(start),
+				err)
+			return nil, status.Errorf(codes.InvalidArgument, err.Error())
+		}
+	}
+
+	// Calls the handler
+	h, err := handler(ctx, req)
+	if err != nil {
+		logger.Log.Infof("Request - Method:%s\tDuration:%s\tError:%v\n",
+			info.FullMethod,
+			time.Since(start),
+			err)
+		return nil, status.Errorf(codes.Unavailable, err.Error())
+	}
+	return h, nil
+}
+
+var (
+	pv, _ = protovalidate.New()
+)
+
+// WithValidatePbServerInterceptor https://github.com/bufbuild/protovalidate-go
+func WithValidatePbServerInterceptor(ctx context.Context,
+	req interface{},
+	info *grpc.UnaryServerInfo,
+	handler grpc.UnaryHandler) (interface{}, error) {
+	start := time.Now()
+	defer func() {
+		logger.Log.Infof("Request - Method:%s\tDuration:%s\t\n",
+			info.FullMethod,
+			time.Since(start))
+	}()
+
+	message, ok := req.(proto.Message)
+	if ok {
+		if err := pv.Validate(message); err != nil {
 			logger.Log.Infof("Request - Method:%s\tDuration:%s\tError:%v\n",
 				info.FullMethod,
 				time.Since(start),
